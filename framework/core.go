eee493fbba85d38f835307db271b21177574b61f
upupnoah <upupqi.cs@gmail.com>
2023-07-13 04:59:21 +0800

    feat: implement custom context for framework project
    This commit implements a custom context for the framework project.
    The custom context includes the following features:
    - Timeout control: Allows setting timeouts for requests.
    - Request/Response data retrieval: Provides functionality to retrieve request and response data.
    - Individual request timeout: Allows setting a timeout for individual requests.

--- framework/core.go
+++ framework/core.go
@@ -1,18 +1,40 @@
 package framework
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 // Core is the core of the framework
 type Core struct {
+	router map[string]ControllerHandler
 }
 
 // NewCore create a new core
 func NewCore() *Core {
-	return new(Core)
+	return &Core{
+		router: map[string]ControllerHandler{},
+	}
+}
+
+func (c *Core) Get(url string, handler ControllerHandler) {
+	c.router[url] = handler
 }
 
 // ServeHTTP implement http.Handler interface
-func (c *Core) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	//TODO implement me
-	panic("implement me")
+func (c *Core) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	log.Println("core.serverHTTP")
+	ctx := NewContext(request, response)
+
+	// 一个简单的路由选择器，这里直接写死为测试路由 foo
+	router := c.router["foo"]
+	if router == nil {
+		return
+	}
+	log.Println("core.router")
+
+	err := router(ctx)
+	if err != nil {
+		return
+	}
 }
